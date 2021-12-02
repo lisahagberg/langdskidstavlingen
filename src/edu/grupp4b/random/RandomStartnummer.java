e60ffd6c423bf3318881f80d7bd1c69d329d0210
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-12-02 11:02:31 +0100

    Add new function to get random startnumber of List

--- src/edu/grupp4b/random/RandomStartnummer.java
+++ src/edu/grupp4b/random/RandomStartnummer.java
@@ -1,5 +1,21 @@
 package edu.grupp4b.random;
 
-public class RandomStartnummer {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class RandomStartnummer {
+	
+	public static void createRandomArrayList() {
+		int mySize = 5;		
+		List<Integer> myList = new ArrayList<Integer>(mySize);
+        for(int i = 1; i <= mySize; i++) {
+        	myList.add(i);
+        }
+        Random rand = new Random();
+        while(myList.size() > 0) {
+            int index = rand.nextInt(myList.size());
+            System.out.println("Testar RandomStartnummer: "+myList.remove(index));
+        }
+	}
 }
