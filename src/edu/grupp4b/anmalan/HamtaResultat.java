445e69b02017d0a45e79ff6d348818bfe7ece03e
Victoria Dahlqvist <elinvictoriad@hotmail.com>
2021-12-06 12:02:33 +0100

    Flyttat menyn till klassen anmälan

--- src/edu/grupp4b/anmalan/HamtaResultat.java
+++ src/edu/grupp4b/anmalan/HamtaResultat.java
@@ -1,5 +1,7 @@
 package edu.grupp4b.anmalan;
 
 public class HamtaResultat {
-
+	
+	// skriv in sitt nr och få reda på vilken resultat man hamnade på
+	
 }
--- src/edu/grupp4b/huvudklass/Main.java
+++ src/edu/grupp4b/huvudklass/Main.java
@@ -1,23 +1,14 @@
 package edu.grupp4b.huvudklass;
 
-import java.util.Scanner;
-
+import edu.grupp4b.anmalan.Anmalan;
 import edu.grupp4b.skidakare.SkapaSkidakare;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Hello, vänligen välj en tävling");
-		System.out.println("[0] Inviduell start\n[1] Masstart\n[2] Jaktstart");
-		System.out.print("Val: ");	
-		int val = scanner.nextInt();
-		if(val == 1) {
-			System.out.println("\n[0] 15 sekunder\n[1] 30 sekunder");
-			System.out.print("Val: ");
-			int mellanrum = scanner.nextInt();
-		}
+
+		Anmalan.registrering();
 		SkapaSkidakare.Skapa();
+		
 	}	
 }
