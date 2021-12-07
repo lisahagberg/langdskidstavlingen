9cb69ab4fc02f1192ed02f02672c63ca07027e9d
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-12-07 15:57:00 +0100

    ordnade till en säkrare menu för anmälan

--- src/edu/grupp4b/tavlingsform/IndividuellStart.java
+++ src/edu/grupp4b/tavlingsform/IndividuellStart.java
@@ -15,12 +15,13 @@ public class IndividuellStart {
 	}
 
 	public void setAntalSekunder(int antalSekunder) {
+		//TODO säkerställ att användaren bara kan mata in 1 eller två.
 		if(antalSekunder == 1) {
 			this.antalSekunder = 15;	
 		} else if(antalSekunder == 2) {
 			this.antalSekunder = 30;
 		} else {
-			System.out.println("felaktig inmatning");
+			System.out.println("felaktig inmatning, välj [1] eller [2]: ");
 		}
 	}
 	
