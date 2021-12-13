2b2b82ebf7c2f9ac39fe7a08370427171a5f52ca
Victoria Dahlqvist <elinvictoriad@hotmail.com>
2021-12-13 13:31:10 +0100

    Startup random skiråkare

---
+++ src/edu/grupp4b/random/AnropaPersoner.java
@@ -0,0 +1,29 @@
+package edu.grupp4b.random;
+
+import java.util.Random;
+
+public class AnropaPersoner {
+	
+	Random random = new Random();
+	
+	String [] randomSkidakare = new String[10]; {
+	randomSkidakare[0] = "Samuel Persson, SWE, Östersunds SK";
+	randomSkidakare[1] = "Britta Johansson, SWE, Östersunds SK";
+	randomSkidakare[2] = "Lina Korsgren, SWE, Tullus IK";
+	randomSkidakare[3] = "Sara Lindborg, SWE, Trönö IK";
+	randomSkidakare[4] = "Anton Karlsson, SWE, Mora SK";
+	randomSkidakare[5] = "Oskar Kardin, SWE, Nacka Värmdö SK ";
+	randomSkidakare[6] = "Öjvind Meijer, NOR, Koteng DNF ";
+	randomSkidakare[7] = "Stian Hoelgaard, NOR, Koteng DNF";
+	randomSkidakare[8] = "Sofie Elebro, NOR, Steinkjer IF";
+	randomSkidakare[9] = "Erica Kveli, FIN, Mäenpää IF";
+	
+	// int randomNumber = random.nextInt(randomSkidakare.length);
+	// String printSkidakare = randomSkidakare[randomNumber];
+	
+	System.out.println();
+	
+	
+	}
+	
+}
