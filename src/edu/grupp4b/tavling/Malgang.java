98ed489f6736e8f519bd8edc8f1b859c46cd0d8b
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-12-14 14:29:23 +0100

    fixed ranking on who crosses the finish line first(not done)

--- src/edu/grupp4b/tavling/Malgang.java
+++ src/edu/grupp4b/tavling/Malgang.java
@@ -7,18 +7,27 @@ import edu.grupp4b.skidakare.SkapaSkidakare;
 import edu.grupp4b.skidakare.Skidakare;
 
 public class Malgang {
-
-	static List<Skidakare> malLista = new ArrayList<>();
 	
-	public static void registreraMal(Skidakare skidakare) {
+	List<Skidakare> malLista = new ArrayList<>();
+	
+	public void registreraMal(Skidakare skidakare) {
 		malLista.add(skidakare);
 		SkapaSkidakare.skidakare.remove(skidakare);
 	}
 	
-	public static void seMal() {
+	public void seMal() {
+		int placering = 1;
 		for(Skidakare lista : malLista) {
-			System.out.println(lista.getFornamn() + lista.getEfternamn() + " med startnummer " + lista.getStartnummer() 
-			+ " gick i mål med tiden: " + lista.getTid());
+			System.out.println(lista.getFornamn() + " " + lista.getEfternamn() 
+			+ " med startnummer " + lista.getStartnummer() + " gick i mål med tiden: " + lista.getTid() 
+			+ " hamnade på " + placering + stringPlacering(placering) + " plats.");
+			placering++;
 		}
+	}
+	private String stringPlacering(int platsNummer) {
+		if(platsNummer == 1 || platsNummer == 2) {
+			return ":a";
+		}
+		return ":e";
 	}
 }
