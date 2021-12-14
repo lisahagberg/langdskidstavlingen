2bfb00c2f8a7e5361dd15b0a68aedc8b35167461
Rasmus Petersson <rasmus.petersson1@yaelev.se>
2021-12-14 22:56:35 +0100

    Refactor Tidtagare

--- src/edu/grupp4b/tidtagare/Tidtagare.java
+++ src/edu/grupp4b/tidtagare/Tidtagare.java
@@ -2,9 +2,11 @@ package edu.grupp4b.tidtagare;
 
 public class Tidtagare {
 
-	private long start;   
+	private long start;
+	private long end;
 	private long tim;
 	private long min;
+	private double milli;
 
 	public Tidtagare() {
 		start = System.nanoTime();
@@ -14,26 +16,57 @@ public class Tidtagare {
 		start = System.nanoTime() - (extraTid * 1000000000);
 	}
 
-	public String getTime() {
-		long passeradTid = System.nanoTime() - start;
-		double centiseconds = (passeradTid / 1000000);
-		centiseconds = centiseconds / 1000;
-		if(centiseconds > 59.999) {
-			centiseconds = centiseconds % 60;
-		}
-		min = passeradTid / 60000000000L;
+	public long getStart() {
+		return start;
+	}
+
+//	public long getEnd() {
+//		return end;
+//	}
+//
+//	public void setEnd(long end) {
+//		this.end = end;
+//	}
+
+	public long getTim() {
+		long passeradTid = System.nanoTime() - getStart();
 		tim = passeradTid / 3600000000000L;
+		return tim;
+	}
+
+
+	public long getMin() {
+		long passeradTid = System.nanoTime() - getStart();
+		min = (passeradTid / 60000000000L);
+		if(min > 59.999) {
+			min = min % 60;
+		}
+		return min;
+	}
+
+	public double getMilli() {
+		long passeradTid = System.nanoTime() - getStart();
+		milli = (passeradTid / 1000000);
+		milli = (milli / 1000);
 		
-		String micro = String.format("%05.2f", centiseconds);
-		micro = micro.replace('.', ':');
-		micro = micro.replace(',', ':');
+		if(milli > 59.999) {
+			milli = milli % 60;
+		}
+		return milli;
+	}
+
+	//Högst nanoTid = snabbast = vinner?
+	public int compareTime() {
+		end = (System.nanoTime() - getStart());
 		
-		String tid = String.format("%02d:%02d:", tim, min) + micro;
-		return tid;
+		return (int) end / 10000;
 	}
 
 	@Override
 	public String toString() {
-		return getTime();
+		String tid = String.format("%02d:%02d:%05.2f", getTim(), getMin(), getMilli());
+		tid = tid.replace('.', ':');
+		tid = tid.replace(',', ':');
+		return tid;
 	}
 }
