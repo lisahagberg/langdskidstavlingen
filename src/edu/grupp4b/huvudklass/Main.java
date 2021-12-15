670b7432751b5138b9c0b5bf785a6bc6b0b2e67f
Oskar Jansson <oskarj92@gmail.com>
2021-12-15 13:58:04 +0100

    Changes in menu structure

--- src/edu/grupp4b/huvudklass/Main.java
+++ src/edu/grupp4b/huvudklass/Main.java
@@ -26,9 +26,6 @@ public class Main {
 		Scanner scanner = new Scanner(System.in);
 		boolean runMenu = true;
 		while (runMenu) {
-			System.out.println("\nAnge startnummer för åkare du vill kontrollera:");
-			System.out.print("Val: ");
-			int startnr = scanner.nextInt();
 			System.out.println("\n[1] Se skidåkarnas tider\n[2] Registrera målgång\n"
 					+ "[3] Se lista över målgång\n[4] System exit");
 			System.out.print("Val: ");
@@ -43,6 +40,9 @@ public class Main {
 //	    	for(int i = 0; i < SkapaSkidakare.skidakare.length; i++) {
 //	    		if(SkapaSkidakare.skidakare[i].getStartnummer() == startnr) {
 //	    			Malgang.registreraMal(SkapaSkidakare.skidakare[i]);
+				System.out.println("\nAnge startnummer för åkare du vill kontrollera:");
+				System.out.print("Val: ");
+				int startnr = scanner.nextInt();
 				
 				for (int i = 0; i < SkapaSkidakare.skidakare.size(); i++) {
 					if (SkapaSkidakare.skidakare.get(i).getStartnummer() == startnr) {
